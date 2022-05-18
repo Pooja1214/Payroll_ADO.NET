7a432f82045bcc08309a7cd14b320c592cdff666
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-18 14:18:06 +0530

    RetrieveAllDetails

---
+++ Emp_PayRoll_ADO.NET/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_PayRoll_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of PayRoll service using ADO.NET");
+            PayRoll_Service payRoll = new PayRoll_Service();
+            payRoll.GetAllData();
+        }
+    }
+}
