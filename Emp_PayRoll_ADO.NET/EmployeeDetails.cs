e6a7006c7c5fa9aab6e07eff820dee776418c74a
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-20 14:17:40 +0530

    WritingDataIntoJsonServer

---
+++ Emp_PayRoll_ADO.NET/EmployeeDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_PayRoll_ADO.NET
+{
+  public  class EmployeeDetails
+    {
+        public int employeeId { get; set; }
+        public int companyId { get; set; }
+        public int departmentId { get; set; }
+        public string employeeName { get; set; }
+        public string startDate { get; set; }
+        public string gender { get; set; }
+        public double phoneNumber { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+
+        public string department { get; set; }
+        public double basicPay { get; set; }
+        public double deduction { get; set; }
+        public double taxablePay { get; set; }
+        public double tax { get; set; }
+        public double netPay { get; set; }
+
+        ////transaction
+        public int isActive { get; set; }
+
+
+    }
+}
