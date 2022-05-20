7870e71c7efc12976cfcf69a3dd40ea4900f1cdf
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-20 14:20:36 +0530

    WritingMultipleDataIntoJsonServer

---
+++ Emp_PayRoll_ADO.NET/PayRollDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_PayRoll_ADO.NET
+{
+    public class PayRollDetails
+    {
+       
+            public double basicPay;
+            public double deduction;
+            public double taxablePay;
+            public double tax;
+            public double netPay;
+            public PayRollDetails(double basicPay)
+            {
+                this.basicPay = basicPay;
+                this.deduction = 0.2 * this.basicPay;
+                this.taxablePay = this.basicPay - this.deduction;
+                this.tax = 0.1 * taxablePay;
+                this.netPay = this.basicPay - this.tax;
+            }
+        }
+    }
---
+++ Emp_PayRoll_ADO.NET/PayRollJsonServer.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_PayRoll_ADO.NET
+{
+    public class PayRollJsonServer
+    {
+        RestClient client;
+        public PayRollJsonServer()
+        {
+            client = new RestClient("http://localhost:3000");
+        }
+        IRestResponse GetAllEmployee()
+        {
+            RestRequest request = new RestRequest("/employees");
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+        public List<EmployeeSalaryDetails> ReadFromServer()
+        {
+            IRestResponse response = GetAllEmployee();
+            var res = JsonConvert.DeserializeObject<List<EmployeeSalaryDetails>>(response.Content);
+            return res;
+        }
+
+        //writing into the server 
+        public void WriteIntoJsonServer(EmployeeSalaryDetails employee)
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            //creating the json object
+            JsonObject json = new JsonObject();
+            //adding the data to json object
+            json.Add("id", employee.id);
+            json.Add("name", employee.name);
+            json.Add("salary", employee.salary);
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            var res = JsonConvert.DeserializeObject<EmployeeSalaryDetails>(response.Content);
+            Console.WriteLine("" + res.id + "Added");
+        }
+
+        //Adding multiple data to the server
+        public void AddingMultipleContactToServer(List<EmployeeSalaryDetails> employees)
+        {
+            foreach (var employee in employees)
+            {
+                WriteIntoJsonServer(employee);
+            }
+        }
+    }
+}
